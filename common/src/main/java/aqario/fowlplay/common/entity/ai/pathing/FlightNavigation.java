556e3c86208f5be874156fd0be126bbe52afd523
aqariio <aquario.mw@gmail.com>
2025-07-29 16:31:52 -0400

    Fix accesswidener not being applied on forge

--- common/src/main/java/aqario/fowlplay/common/entity/ai/pathing/FlightNavigation.java
+++ common/src/main/java/aqario/fowlplay/common/entity/ai/pathing/FlightNavigation.java
@@ -3,10 +3,7 @@ package aqario.fowlplay.common.entity.ai.pathing;
 import aqario.fowlplay.common.entity.FlyingBirdEntity;
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ai.pathing.BirdPathNodeMaker;
-import net.minecraft.entity.ai.pathing.MobNavigation;
-import net.minecraft.entity.ai.pathing.Path;
-import net.minecraft.entity.ai.pathing.PathNodeNavigator;
+import net.minecraft.entity.ai.pathing.*;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.server.network.DebugInfoSender;
 import net.minecraft.util.math.BlockPos;
@@ -61,12 +58,18 @@ public class FlightNavigation extends MobNavigation implements ExtendedNavigator
             return null;
         }
 
-        return new Path(path.nodes, path.getTarget(), path.reachesTarget()) {
+        Path newPath = new Path(path.nodes, path.getTarget(), path.reachesTarget()) {
             @Override
             public Vec3d getNodePosition(Entity entity1, int nodeIndex) {
                 return FlightNavigation.this.getEntityPosAtNode(nodeIndex);
             }
         };
+        PathNode[] debugNodes = path.getDebugNodes();
+        PathNode[] debugSecondNodes = path.getDebugSecondNodes();
+        Set<TargetPathNode> debugTargetNodes = path.debugTargetNodes;
+        newPath.setDebugInfo(debugNodes, debugSecondNodes, debugTargetNodes);
+
+        return newPath;
     }
 
     @Override
--- common/src/main/java/aqario/fowlplay/common/entity/ai/pathing/GroundNavigation.java
+++ common/src/main/java/aqario/fowlplay/common/entity/ai/pathing/GroundNavigation.java
@@ -2,15 +2,56 @@ package aqario.fowlplay.common.entity.ai.pathing;
 
 import aqario.fowlplay.common.entity.FlyingBirdEntity;
 import aqario.fowlplay.common.util.Birds;
-import net.minecraft.entity.ai.pathing.Path;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.pathing.*;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.ChunkCache;
 import net.tslat.smartbrainlib.api.core.navigation.SmoothGroundNavigation;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
 
 public class GroundNavigation extends SmoothGroundNavigation {
     public GroundNavigation(MobEntity mob, World level) {
         super(mob, level);
+    }
+
+    @Override
+    protected PathNodeNavigator createPathNodeNavigator(int maxVisitedNodes) {
+        this.nodeMaker = new BirdPathNodeMaker();
+        this.nodeMaker.setCanEnterOpenDoors(true);
+
+        return new PathNodeNavigator(this.nodeMaker, maxVisitedNodes) {
+            @Nullable
+            @Override
+            public Path findPathToAny(ChunkCache navigationRegion, MobEntity mob, Set<BlockPos> targetPositions, float maxRange, int accuracy, float searchDepthMultiplier) {
+                final Path path = super.findPathToAny(navigationRegion, mob, targetPositions, maxRange, accuracy, searchDepthMultiplier);
+
+                return GroundNavigation.this.patchPath(path);
+            }
+        };
+    }
+
+    public @Nullable Path patchPath(@Nullable Path path) {
+        if(path == null) {
+            return null;
+        }
+
+        Path newPath = new Path(path.nodes, path.getTarget(), path.reachesTarget()) {
+            @Override
+            public Vec3d getNodePosition(Entity entity1, int nodeIndex) {
+                return GroundNavigation.this.getEntityPosAtNode(nodeIndex);
+            }
+        };
+        PathNode[] debugNodes = path.getDebugNodes();
+        PathNode[] debugSecondNodes = path.getDebugSecondNodes();
+        Set<TargetPathNode> debugTargetNodes = path.debugTargetNodes;
+        newPath.setDebugInfo(debugNodes, debugSecondNodes, debugTargetNodes);
+
+        return newPath;
     }
 
     @Override

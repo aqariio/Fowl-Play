2a4a22a8f001606f3bb8f60d0ca0747d9184b129
aqariio <aquario.mw@gmail.com>
2026-03-17 21:48:29 -0400

    Add bird feeder interaction

--- common/src/main/java/aqario/fowlplay/core/FowlPlayBuiltInRegistries.java
+++ common/src/main/java/aqario/fowlplay/core/FowlPlayBuiltInRegistries.java
@@ -4,7 +4,7 @@ import aqario.fowlplay.common.entity.variant.*;
 import aqario.fowlplay.common.util.RegistryBuilder;
 import net.minecraft.core.Registry;
 
-public class FowlPlayBuiltInRegistries {
+public final class FowlPlayBuiltInRegistries {
     public static final Registry<ChickenVariant> CHICKEN_VARIANT = RegistryBuilder
         .create(FowlPlayRegistries.CHICKEN_VARIANT)
         .sync()

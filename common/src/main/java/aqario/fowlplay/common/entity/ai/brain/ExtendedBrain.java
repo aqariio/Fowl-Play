77a37facd140d79ad1920f0a3180ceef5596d988
aqariio <aquario.mw@gmail.com>
2026-04-20 15:16:23 -0400

    Add smartbrain extension

---
+++ common/src/main/java/aqario/fowlplay/common/entity/ai/brain/ExtendedBrain.java
@@ -0,0 +1,63 @@
+package aqario.fowlplay.common.entity.ai.brain;
+
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.behavior.Behavior;
+import net.minecraft.world.entity.ai.behavior.BehaviorControl;
+import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.schedule.Activity;
+import net.tslat.smartbrainlib.api.SmartBrainOwner;
+import net.tslat.smartbrainlib.api.core.BrainActivityGroup;
+import net.tslat.smartbrainlib.api.core.SmartBrain;
+import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
+import net.tslat.smartbrainlib.util.BrainUtils;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ExtendedBrain<E extends LivingEntity & SmartBrainOwner<E>> extends SmartBrain<E> {
+    public ExtendedBrain(List<MemoryModuleType<?>> memories, List<? extends ExtendedSensor<E>> extendedSensors, @Nullable List<BrainActivityGroup<E>> taskList) {
+        super(memories, extendedSensors, taskList);
+    }
+
+    @Override
+    public void tick(ServerLevel level, E entity) {
+        entity.level().getProfiler().push("ExtendedBrain");
+
+        if(this.sortBehaviours) {
+            this.behaviours.sort(Comparator.comparingInt(ActivityBehaviours::priority));
+        }
+
+        this.forgetOutdatedMemories();
+        this.tickSensors(level, entity);
+        this.checkForNewBehaviours(level, entity);
+        this.tickRunningBehaviours(level, entity);
+        this.findAndSetActiveActivity(entity);
+        this.stopBehavioursFromInactiveActivity(level, entity);
+
+        entity.level().getProfiler().pop();
+
+        if(entity instanceof Mob mob) {
+            mob.setAggressive(BrainUtils.hasMemory(mob, MemoryModuleType.ATTACK_TARGET));
+        }
+    }
+
+    protected void stopBehavioursFromInactiveActivity(ServerLevel level, E entity) {
+        long gameTime = level.getGameTime();
+
+        for(ActivityBehaviours<E> behaviourGroup : this.behaviours) {
+            for(Pair<Activity, List<BehaviorControl<? super E>>> pair : behaviourGroup.behaviours()) {
+                if(!this.getActiveActivities().contains(pair.getFirst())) {
+                    for(BehaviorControl<? super E> behaviour : pair.getSecond()) {
+                        if(behaviour.getStatus() == Behavior.Status.RUNNING) {
+                            behaviour.doStop(level, entity, gameTime);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
